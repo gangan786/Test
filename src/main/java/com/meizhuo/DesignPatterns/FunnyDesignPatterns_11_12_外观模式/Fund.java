e6b987a0118414baacf3ab5134a6d0caca0c1311
gangan786 <215387011@qq.com>
2019-01-18 23:29:20 +0800

    Image add

---
+++ src/main/java/com/meizhuo/DesignPatterns/FunnyDesignPatterns_11_12_外观模式/Fund.java
@@ -0,0 +1,17 @@
+package com.meizhuo.DesignPatterns.FunnyDesignPatterns_11_12_外观模式;
+
+/**
+ * @ProjectName: Test
+ * @Package: com.meizhuo.DesignPatterns.FunnyDesignPatterns_11_12_外观模式
+ * @ClassName: ${TYPE_NAME}
+ * @Description:
+ * @Author: Gangan
+ * @CreateDate: 2019/1/18 23:00
+ * @UpdateUser:
+ * @UpdateDate: 2019/1/18 23:00
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ * <p>Copyright: Copyright (c) 2019</p>
+ */
+public class Fund {
+}

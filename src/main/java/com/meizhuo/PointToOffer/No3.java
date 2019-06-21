8c6f326671c198fb64973621505d8ddb61d37980
gangan786 <ganganmaster@163.com>
2019-06-21 17:22:58 +0800

    PointToOffer  -->  二分法找出重复数字

--- src/main/java/com/meizhuo/PointToOffer/No3.java
+++ src/main/java/com/meizhuo/PointToOffer/No3.java
@@ -24,11 +24,11 @@ public class No3 {
 
             while (i != valus[i]) {
                 if (valus[valus[i]] != valus[i]) {
-                    int temp=valus[valus[i]];
-                    valus[valus[i]]=valus[i];
-                    valus[i]=temp;
+                    int temp = valus[valus[i]];
+                    valus[valus[i]] = valus[i];
+                    valus[i] = temp;
                 } else {
-                    System.out.println("其中一个重复数字为："+valus[i]);
+                    System.out.println("其中一个重复数字为：" + valus[i]);
                     return true;
                 }
             }
@@ -39,11 +39,47 @@ public class No3 {
         return false;
     }
 
+    public int halfDuplicate(int[] values) {
+        int start = 1;
+        int end = values.length - 1;
+        while (start <= end) {
+            int mid = ((end - start) >> 1) + start;
+            int count = countElm(values, start, mid);
+
+            if (start == end) {
+                if (count > 1) {
+                    //找到重复的一个数字
+                    return start;
+                } else {
+                    return -1;
+                }
+            }
+
+            if (count == (mid - start + 1)) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+
+        }
+        return -1;
+    }
+
+    private int countElm(int[] values, int start, int mid) {
+        int count = 0;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] >= start && values[i] <= mid) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     @Test
     public void test() {
         int[] ints = {2, 6, 1, 0, 4, 5, 3};
-        Integer integer = new Integer(-1);
-        System.out.println("结果为：" + duplicate(ints));
+        int[] halfTest = {2, 1, 5, 4, 3, 2, 6, 7};
+        System.out.println("结果为：" + halfDuplicate(halfTest));
     }
 
 }

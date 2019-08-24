8b109896be34cf02cfe1710d576a2b880c6e8e41
gangan786 <ganganmaster@163.com>
2019-08-24 22:35:52 +0800

    京东笔试 GG

---
+++ src/main/java/com/meizhuo/temp/No18.java
@@ -0,0 +1,25 @@
+package com.meizhuo.temp;
+
+import java.util.Scanner;
+
+/**
+ * @Classname No18
+ * @Description 合唱队形
+ * @Date 2019/8/24 19:37
+ * @Created by Gangan
+ */
+public class No18 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        int[] heights = new int[count];
+
+        for (int i = 0; i < heights.length; i++) {
+            heights[i]=scanner.nextInt();
+        }
+
+
+    }
+
+}
